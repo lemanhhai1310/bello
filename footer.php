b4b94243c2624f80d69f2c79a7d171f9e191bb04
Le Manh Hai <hai.lemanh.1310@gmail.com>
2019-07-27 11:32:44 +0700

    khởi tạo project

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer id="bello-footer">
+    <div class="bello-top-f uk-text-center uk-section-xsmall">
+        <div class="uk-container">
+            <figure><a href="#"><img src="imgs/logo-bello-f.png" alt=""></a></figure>
+            <address>
+                <div>•  Ngày : 7,8,9/08/2019</div>
+                <div>• Địa chỉ : cửa hàng 33 Hàng Khay, Hà Nội</div>
+            </address>
+        </div>
+    </div>
+    <div class="bello-bottom-f uk-text-center">
+        <div class="uk-container">
+            © 2019 by Bello. All Rights Reserved.
+        </div>
+    </div>
+</footer>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "header.php"; ?>
+<?php include "block-sp.php"; ?>
+<?php include "block-01.php"; ?>
+<?php include "block-dky.php"; ?>
+<?php include "block-kh.php"; ?>
+<?php include "footer.php"; ?>
